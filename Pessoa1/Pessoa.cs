9e4db459004ca58888a1a058ff2bb162cfa6ad95
Johnnyboi3020 <joaovitorzampier09@gmail.com>
2021-08-05 13:52:34 -0300

    Adicionar arquivos de projeto.

---
+++ Pessoa1/Pessoa.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pessoa1
+{
+    class Pessoa
+    {
+        public string Nome1 { get; set; }
+        public byte Idade1 { get; set; }
+        public string Nome2 { get; set; }
+        public byte Idade2 { get; set; }
+
+        public string Maior()
+        {
+           if (Idade1 > Idade2)
+           {
+               return Nome1;
+           }
+           else
+           {
+               return Nome2;
+           }
+        }
+
+        public override string ToString()
+        {
+            return $"Pessoa mais velha: {Maior()}";
+        }
+    }
+}
---
+++ Pessoa1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Pessoa1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p = new Pessoa();
+
+            Console.WriteLine("Dados da primeira pessoa:");
+
+            Console.WriteLine("Nome:");
+            p.Nome1 = Console.ReadLine();
+
+            Console.WriteLine("Idade:");
+            p.Idade1 = byte.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dados da segunda pessoa:");
+
+            Console.WriteLine("Nome:");
+            p.Nome2 = Console.ReadLine();
+
+            Console.WriteLine("Idade:");
+            p.Idade2 = byte.Parse(Console.ReadLine());
+
+            Console.WriteLine(p);
+        }
+    }
+}
